6e812d4aa59840a348c0d334ac1c6b523ec7c4cc
Taylor Mason <taylorwmason@gmail.com>
2023-04-30 21:26:27 -0400

    inital commit

---
+++ components/SideNav.vue
@@ -0,0 +1,21 @@
+<script setup>
+const links = [
+		{to: '/', title: 'Home'},
+		{to: '/services', title: 'Services'},
+		{to: '/work', title: 'Our Work'},
+		{to: '/about', title: 'About Us'},
+		{to: '/contact', title: 'Contact'}
+	]
+</script>
+<template>
+	<nav>
+		<ul>
+			<li v-for="link in links" class="transition-all hover:translate-x-6">
+				<NuxtLink :to="link.to">{{ link.title }}</NuxtLink>
+			</li>
+		</ul>
+	</nav>
+</template>
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<main class="">
+		<section class="full-sec text-white bg-black flex">
+			<div class="absolute left-4 top-16">
+				<SideNav/>
+			</div>
+			<div class="flex text-white flex-col mx-auto justify-center items-center">
+				<h1>we're filmit.xyz</h1>
+				<h3>we make videos, design stuff, and really enjoy it.</h3>
+				<h3>most of the time we call ourselves an agency</h3>
+			</div>
+		</section>
+	</main>
+</template>
+<style>
+	body{
+		margin: 0;
+		font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+	}
+	h1{
+		font-size: 4.5rem;
+		display: inline;
+		color: white
+	}
+	h3{
+		font-weight: 300;
+	}
+	.full-sec{
+   width: 100vw;
+	 height: 100vh;
+	}
+</style>
