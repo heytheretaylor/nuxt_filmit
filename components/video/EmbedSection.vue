dc0f6144f996dd10f96a2c6e6506688b15b6fb0b
Taylor Mason <taylorwmason@gmail.com>
2023-05-11 08:50:36 +0100

    added videos

--- components/video/EmbedSection.vue
+++ components/video/EmbedSection.vue
@@ -1,7 +1,19 @@
 <template>
-	<section>
+	<section class="w-full">
+		<div 
+		style="padding:56.25% 0 0 0;position:relative;"
+		>
+			<iframe 
+				:src="src" 
+				frameborder="0" 
+				allow="autoplay; fullscreen; picture-in-picture" 
+				allowfullscreen 
+				style="position:absolute;top:0;left:0;width:100%;height:100%;" 
+				:title="props.title"
+				>
+			</iframe>
+		</div>
 
-		<slot></slot>
 		<div>{{ props.title }}</div>
 	</section>
 </template>
@@ -10,6 +22,16 @@ const props = defineProps({
 	title: {
 		type: String,
 		required: true
+	},
+	videoId: {
+		type: String,
+		required: true
 	}
 })
+const src = computed(()=> {
+	return `https://player.vimeo.com/video/${props.videoId}&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479`
+})
+// onMounted(() => {
+// 	player.value = new Player(videoContainer.value, {id: props.videoId})
+// 	})
 </script>
--- pages/work.vue
+++ pages/work.vue
@@ -1,6 +1,27 @@
 <template>
-	<main class="flex flex-col items-center my-10">
+	<main class="flex flex-col items-center my-10 mx-24">
 		<SideNav></SideNav>
-		<VideoEmbedSection title="Bike Ride Test"></VideoEmbedSection>
+		<VideoEmbedSection v-for="video in videos" :title="video.title" :video-id="video.id">
+		</VideoEmbedSection>
 	</main>
 </template>
+<script setup>
+	const videos = [
+		{
+			title: 'Bike Ride Test | Red Helium',
+			id: '728611217?h=380144f2fe'
+		},
+		{
+			title: 'Walking Sequence',
+			id: "725880927?h=1675ccc83d"
+		},
+		{
+			title: 'World Tour 3.0',
+			id: "728923199?h=ae0bf44259"
+		},
+		{
+			title: 'Charleston Aerials',
+			id: "589119705?h=cbcf919dbd"
+		}
+	]
+</script>
